a3ce438e03baba6a93e33305f2cff24fd5367b09
IckyTheBiggy <elcia116eb@gmail.com>
2024-03-26 14:42:51 +0000

    Added the editor

---
+++ Project/Unity/PortalShift/Assets/Scripts/UI/PlacementMenuScript.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts.UI
+{
+    public class PlacementMenuScript : MonoBehaviour
+    {
+        [System.Serializable]
+        public struct PlaceableItem
+        {
+            public string Name;
+            public GameObject Prefab;
+        }
+
+        [SerializeField] private List<PlaceableItem> _placeableItems;
+        [SerializeField] private PlacemenuItemScript _placemenuItemPrefab;
+        [SerializeField] private Transform _placeMenuUI;
+
+        private void Start()
+        {
+            foreach (var item in _placeableItems)
+            {
+                var menuItem =
+                    Instantiate(_placemenuItemPrefab, _placeMenuUI);
+                menuItem.AssignInfo(item.Name, item.Prefab);
+            }
+        }
+    }
+}
