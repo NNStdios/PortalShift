5badcf330ea089f90b7c10452ad33247818d695f
EvilGen3301 <tablock18@gmail.com>
2024-03-27 14:16:11 +0400

    jumpPad with directions

--- Project/Unity/PortalShift/Assets/Scripts/World/JumpPadScript.cs
+++ Project/Unity/PortalShift/Assets/Scripts/World/JumpPadScript.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
 public class JumpPadScript : MonoBehaviour
 {
-    [SerializeField] private Vector2 _force;
+    public float jumpForce = 3f;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!collision.TryGetComponent<Rigidbody2D>(out var rb)) 
             return;
 
         rb.velocity = Vector2.zero;
-        rb.AddForce(_force, ForceMode2D.Impulse);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+
     }
+
 }
+ 
